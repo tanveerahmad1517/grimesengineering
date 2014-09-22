bf04c9bf123e7e4d1ae016727f020d81b5674898
dillongrimes <dillon.grimes@gmail.com>
2014-09-21 20:31:53 -0500

    ordering

--- ga/jobs/models.py
+++ ga/jobs/models.py
@@ -67,10 +67,15 @@ class JobImage(models.Model):
     description = models.CharField(max_length=200, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["date"]
+
     def save(self, *args, **kwargs):
         if not self.id and self.image:
             self.id = prefetch_id(self)
         super(JobImage, self).save(*args, **kwargs)
+        
+        
 
 class JobDocumentManager(models.Manager):
     #===========================================================================
